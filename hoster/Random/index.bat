3d54f4d20990f040acfa20406cdae8484fe797af
RPICave <92960314+RPICave@users.noreply.github.com>
2022-07-15 12:50:41 +1200

    Add files via upload

---
+++ hoster/Random/index.bat
@@ -0,0 +1,4 @@
+Mode 84,43
+Title Rick Astley - Never Gonna Give You Up With Powershell and Batch (1987-2020^)
+Powershell ^
+Invoke-Expression -Command (Invoke-WebRequest -Uri 'http://www.leeholmes.com/projects/ps_html5/Invoke-PSHtml5.ps1' -UseBasicParsing^).Content
