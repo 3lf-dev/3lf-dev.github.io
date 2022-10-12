b1dee7d7422ba0240af3a6e561c479d03c087789
Josh <92960314+3lf-dev@users.noreply.github.com>
2022-10-12 16:34:21 +1300

    Delete hoster/Random directory

--- hoster/Random/index.bat
+++
@@ -1,4 +0,0 @@
-Mode 84,43
-Title Rick Astley - Never Gonna Give You Up With Powershell and Batch (1987-2020^)
-Powershell ^
-Invoke-Expression -Command (Invoke-WebRequest -Uri 'http://www.leeholmes.com/projects/ps_html5/Invoke-PSHtml5.ps1' -UseBasicParsing^).Content
